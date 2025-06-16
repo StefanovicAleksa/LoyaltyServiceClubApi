94767115687cac08e5c81052fc688c2b84d67fee
Stefanovic Aleksa <aleksa.stefanovic0223@gmail.com>
2025-06-16 19:44:51 +0200

    🛠️ added username field to the accounts table

--- src/main/resources/db/migration/tables/V014__create_customer_accounts_table.sql
+++ src/main/resources/db/migration/tables/V014__create_customer_accounts_table.sql
@@ -4,11 +4,12 @@
 CREATE TABLE customer_accounts
 (
     id                  BIGSERIAL PRIMARY KEY,
-    customer_id         BIGINT UNIQUE                             NOT NULL,
+    username            VARCHAR(60) UNIQUE                        NOT NULL,
     password            VARCHAR(255)                              NOT NULL,
     activity_status     customer_account_activity_status_enum     NOT NULL DEFAULT 'ACTIVE',
     verification_status customer_account_verification_status_enum NOT NULL DEFAULT 'UNVERIFIED',
     last_login_at       TIMESTAMPTZ,
+    customer_id         BIGINT UNIQUE                             NOT NULL,
 
     -- spring audit columns
     created_date        TIMESTAMPTZ                               NOT NULL DEFAULT CURRENT_TIMESTAMP,
