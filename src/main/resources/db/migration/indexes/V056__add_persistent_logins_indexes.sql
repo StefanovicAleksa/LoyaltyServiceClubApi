35f8c461fca450ae2e745f0bb23d8ca69f0c7c57
Stefanovic Aleksa <aleksa.stefanovic0223@gmail.com>
2025-06-18 02:56:42 +0200

    🛠️ removed foreign key to customer account table from persistent login table ot make it have default expected spring pattern. also commited the removes

--- src/main/resources/db/migration/indexes/V056__add_persistent_logins_indexes.sql
+++
@@ -1,5 +0,0 @@
--- V056__add_persistent_logins_indexes.sql
--- Indexes for persistent_logins table
-
--- Foreign key index for easier cleanup when customer accounts are deleted
-CREATE INDEX idx_persistent_logins_customer_account_id ON persistent_logins(customer_account_id);
--- src/main/resources/db/migration/tables/V016__create_persistent_logins_table.sql
+++ src/main/resources/db/migration/tables/V016__create_persistent_logins_table.sql
@@ -6,12 +6,5 @@ CREATE TABLE persistent_logins
     series              VARCHAR(64) PRIMARY KEY,
     username            VARCHAR(64) NOT NULL, -- email of customer account
     token               VARCHAR(64) NOT NULL,
-    last_used           TIMESTAMP   NOT NULL,
-
-    customer_account_id BIGINT,
-    CONSTRAINT fk_persistent_login_customer_account
-        FOREIGN KEY (customer_account_id)
-            REFERENCES customer_accounts (id)
-            ON UPDATE CASCADE
-            ON DELETE CASCADE
+    last_used           TIMESTAMP   NOT NULL
 );
