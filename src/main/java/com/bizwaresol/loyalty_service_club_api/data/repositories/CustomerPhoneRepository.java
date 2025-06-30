3e79e6229ea3fb6d5716e917d01a3ad00bc069ce
Stefanovic Aleksa <aleksa.stefanovic0223@gmail.com>
2025-06-30 17:05:37 +0200

    🚀 dao repositories for database tables

---
+++ src/main/java/com/bizwaresol/loyalty_service_club_api/data/repositories/CustomerPhoneRepository.java
@@ -0,0 +1,20 @@
+package com.bizwaresol.loyalty_service_club_api.data.repositories;
+
+import com.bizwaresol.loyalty_service_club_api.domain.entity.CustomerPhone;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CustomerPhoneRepository extends JpaRepository<CustomerPhone, Long> {
+    Optional<CustomerPhone> findByPhone(String phoneNumber);
+
+    boolean existsByPhone(String phoneNumber);
+
+    List<CustomerPhone> findByVerified(boolean isVerified);
+
+    long countByVerified(boolean isVerified);
+}
